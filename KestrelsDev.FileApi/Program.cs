6345fd7dda284c4b39a6bf0460e0663cf9a5733a
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2026-01-01 21:04:30 +0100

    revisting on how to do this

--- KestrelsDev.FileApi/Program.cs
+++ KestrelsDev.FileApi/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using KestrelsDev.FileApi.Services.ConfigurationService;
 using KestrelsDev.FileApi.Services.ChecksumService;
 using KestrelsDev.FileApi.Services.FileStorageService;
